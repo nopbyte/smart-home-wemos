98ceb58f84a21b2a49d02ef46dcbb47244c85fd8
david <nopbyte>
2017-02-05 22:21:36 +0100

    add configuration sample

---
+++ configuration_sample.h
@@ -0,0 +1,8 @@
+#define SSID "yourSSID"
+#define WIFIPASS "yourWifiPass"
+#define SERVER "yourDataserverIP"
+#define PORT 9090 //your server port
+#define DATAUSERNAME "influxdb_username_here"
+#define DATAPASSWORD "influxdb_password_here"
+#define DBNAME "database_name_here"
+
